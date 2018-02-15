915e0d94aebe3ecd3ffcc9a8a378f1bc3129517f
Josh Palmeri <palmerijosh@gmail.com>
2018-02-14 19:53:03 -0500

    Added geometric shapes and hover changes

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 $(document).ready(function() {
-	$('.color-trigger').hover(function() {
+	$('.color-trigger, img.main-logo').hover(function() {
 		var color = '#'+$(this).attr('data-color');
 		$('.site-wrap').css('background',color);
+		var imgClass = '.'+$(this).attr('data-shape');
+		$('.lg-img-wrapper img').addClass('hide-accessible');
+		$(imgClass).removeClass('hide-accessible');
 	});
 });
